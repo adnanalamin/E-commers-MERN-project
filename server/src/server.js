e5a1618edaac77e06c5a3f8dcf1b893fec53da42
Al-Amin <alaminadnan070@gmail.com>
2023-05-30 14:34:07 +0600

    update project

--- server/src/server.js
+++ server/src/server.js
@@ -5,9 +5,24 @@ const app = express();
 
 app.use(morgan('dev'));
 
+const isLoggedIn = (req,res,next)=>{
+	const logIn = false;
+	if (logIn) {
+		next();
+	} else{
+		return res.status(401).json({message:"Login Faild"})
+	};
+};
+
 app.get('/test',(req,res)=>{
 	res.status(200).send({
 		message:"api is working!",
+	})
+});
+app.post('/test',isLoggedIn,(req,res)=>{
+	console.log("User Profile");
+	res.status(200).send({
+		message:"put api is working!",
 	})
 });
 
